c341108216759eb8a8a7cfbbccbe0c5f163edf13
johnconh <sevenballs@gmail.com>
2024-06-18 18:54:45 +0200

    headers

--- src/checkGLError.cpp
+++ src/checkGLError.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checkGLError.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdasilva <jdasilva@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-06-18 16:54:05 by jdasilva          #+#    #+#             */
+/*   Updated: 2024-06-18 16:54:05 by jdasilva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/checkGLError.h"
 
 void checkGLError(const char* statement, const char* file, int line) {
