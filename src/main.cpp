8c9e24fb42e02508dca3af5fc5d443adaa3788c8
johnconh <sevenballs@gmail.com>
2024-06-18 17:54:00 +0200

    42 en colores

--- src/main.cpp
+++ src/main.cpp
@@ -79,68 +79,97 @@ int main(int argc, char **argv)
     std::vector<Vertex> vertices;
     std::vector<Face> faces;
     loadOBJ(objFile.c_str(), vertices, faces);
-    std::vector<float> vertexData ;
-    for(const Face& face : faces)
+    std::vector<float> vertexData;
+    std::vector<float> colorData;
+    for(size_t faceIndex = 0; faceIndex < faces.size(); faceIndex++)
     {
+        const Face& face = faces[faceIndex];
+
+        float r = static_cast <float> (faceIndex % 3 == 0);
+        float g = static_cast <float> (faceIndex % 3 == 1);
+        float b = static_cast <float> (faceIndex % 3 == 2);
+    
         for(size_t i = 0; i < face.v.size() - 2; i++)
         {
             vertexData.push_back(vertices[face.v[0] - 1].x);
             vertexData.push_back(vertices[face.v[0] - 1].y);
             vertexData.push_back(vertices[face.v[0] - 1].z);
+            colorData.push_back(r);
+            colorData.push_back(g);
+            colorData.push_back(b);
 
             vertexData.push_back(vertices[face.v[i + 1] - 1].x);
             vertexData.push_back(vertices[face.v[i + 1] - 1].y);
             vertexData.push_back(vertices[face.v[i + 1] - 1].z);
+            colorData.push_back(r);
+            colorData.push_back(g);
+            colorData.push_back(b);
 
             vertexData.push_back(vertices[face.v[i + 2] - 1].x);
             vertexData.push_back(vertices[face.v[i + 2] - 1].y);
             vertexData.push_back(vertices[face.v[i + 2] - 1].z);
+            colorData.push_back(r);
+            colorData.push_back(g);
+            colorData.push_back(b);
         }
     }
 
-
     GLuint shaderProgram = createShaderProgram("res/shaders/vertexShader.glsl", "res/shaders/fragmentShader.glsl");
 
-    GLuint VBO, VAO;
+    GLuint VBO[2], VAO;
     CHECK_GL_ERROR(glGenVertexArrays(1, &VAO));
-    CHECK_GL_ERROR(glGenBuffers(1, &VBO));
+    CHECK_GL_ERROR(glGenBuffers(2, VBO));
 
     CHECK_GL_ERROR(glBindVertexArray(VAO));
 
-    CHECK_GL_ERROR(glBindBuffer(GL_ARRAY_BUFFER, VBO));
+    CHECK_GL_ERROR(glBindBuffer(GL_ARRAY_BUFFER, VBO[0]));
     CHECK_GL_ERROR(glBufferData(GL_ARRAY_BUFFER, vertexData.size() * sizeof(float), vertexData.data(), GL_STATIC_DRAW));
-
     CHECK_GL_ERROR(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0));
     CHECK_GL_ERROR(glEnableVertexAttribArray(0));
+
+    CHECK_GL_ERROR(glBindBuffer(GL_ARRAY_BUFFER, VBO[1]));
+    CHECK_GL_ERROR(glBufferData(GL_ARRAY_BUFFER, colorData.size() * sizeof(float), colorData.data(), GL_STATIC_DRAW));
+    CHECK_GL_ERROR(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0));
+    CHECK_GL_ERROR(glEnableVertexAttribArray(1));
 
     CHECK_GL_ERROR(glBindBuffer(GL_ARRAY_BUFFER, 0));
     CHECK_GL_ERROR(glBindVertexArray(0));
 
-    
-    GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
-    GLint viewLoc = glGetUniformLocation(shaderProgram, "view");
-    GLint projLoc = glGetUniformLocation(shaderProgram, "projection");
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+    glm::mat4 model = glm::mat4(1.0f);
 
     while(!glfwWindowShouldClose(window))
     {
-        float rotationAngle = glfwGetTime();
-        glm::mat4 model = glm::rotate(glm::mat4(1.0f), rotationAngle, glm::vec3(0.0f, 1.0f, 0.0f));
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         CHECK_GL_ERROR(glClear(GL_COLOR_BUFFER_BIT));
 
-        CHECK_GL_ERROR(glBindVertexArray(VAO));
         CHECK_GL_ERROR(glUseProgram(shaderProgram));
+
+        model = glm::rotate(model, glm::radians(0.5f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        GLint modelLoc = glGetUniformLocation(shaderProgram, "model");
+        GLint viewLoc = glGetUniformLocation(shaderProgram, "view");
+        GLint projLoc = glGetUniformLocation(shaderProgram, "projection");
+
+        if (modelLoc == -1 || viewLoc == -1 || projLoc == -1) 
+        {
+            std::cerr << "Error: Failed to get uniform locations" << std::endl;
+            return -1;
+        }
+
         CHECK_GL_ERROR(glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model)));
         CHECK_GL_ERROR(glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view)));
         CHECK_GL_ERROR(glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection)));
+
+        CHECK_GL_ERROR(glBindVertexArray(VAO));
         CHECK_GL_ERROR(glDrawArrays(GL_TRIANGLES, 0, vertexData.size() / 3));
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     CHECK_GL_ERROR(glDeleteVertexArrays(1, &VAO));
-    CHECK_GL_ERROR(glDeleteBuffers(1, &VBO));
+    CHECK_GL_ERROR(glDeleteBuffers(2, VBO));
     CHECK_GL_ERROR(glDeleteProgram(shaderProgram));
     glfwTerminate();
     return 0;
